fdd21e7623d192dc7f2b1b5606ba9722b61a1912
Maddoge-oss <68809857+Maddoge-oss@users.noreply.github.com>
2020-08-02 23:47:10 +0700

    Delete data.php

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-   $data_file = fopen("example.txt", "w");
-   $confession = $_POST[""];
-   $text_to_write = $confession;
-
-   fwrite($data_file, $text_to_write);
-   fclose($data_file)
-?>
